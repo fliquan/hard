3efa2ea71998af048ce5b8985cd418aceb20ccf9
flq <flq@U2matoMacBook-Pro.local>
2016-11-20 21:57:15 +0800

    分布式服务

---
+++ 分布式服务/Zookeeper.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1479648676311" ID="ID_815715148" MODIFIED="1479648687882" TEXT="Zookeeper">
+<node CREATED="1479648693943" ID="ID_1792213094" MODIFIED="1479648706092" POSITION="right" TEXT="&#x4f7f;&#x7528;&#x573a;&#x666f;">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</map>
