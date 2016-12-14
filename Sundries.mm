ab149fac0d13c552ff8712d41f26d9ab2c22c5ba
unknown <Administrator@3XX4VY1.comtop.local>
2016-12-14 09:43:57 +0800

    c

---
+++ Sundries.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1481513886378" ID="ID_845800660" MODIFIED="1481513895446" TEXT="Sundries">
+<node CREATED="1481513896030" ID="ID_290876719" MODIFIED="1481513899148" POSITION="right" TEXT="yaml">
+<node CREATED="1481513899149" ID="ID_1010103247" MODIFIED="1481513900973" TEXT="YAML&#x662f;&#x4e00;&#x79cd;&#x4eba;&#x4eec;&#x53ef;&#x4ee5;&#x8f7b;&#x677e;&#x9605;&#x8bfb;&#x7684;&#x6570;&#x636e;&#x5e8f;&#x5217;&#x5316;&#x683c;&#x5f0f;&#xff0c;&#x5e76;&#x4e14;&#x5b83;&#x975e;&#x5e38;&#x9002;&#x5408;&#x5bf9;&#x52a8;&#x6001;&#x7f16;&#x7a0b;&#x8bed;&#x8a00;&#x4e2d;&#x4f7f;&#x7528;&#x7684;&#x6570;&#x636e;&#x7c7b;&#x578b;&#x8fdb;&#x884c;&#x7f16;&#x7801;&#x3002;YAML&#x662f;YAML Ain&apos;t Markup Language&#x7b80;&#x5199;&#xff0c;&#x548c;GNU(&quot;GNU&apos;s Not Unix!&quot;)&#x4e00;&#x6837;&#xff0c;YAML&#x662f;&#x4e00;&#x4e2a;&#x9012;&#x5f52;&#x7740;&#x8bf4;&#x201c;&#x4e0d;&#x201d;&#x7684;&#x540d;&#x5b57;&#x3002;&#x4e0d;&#x540c;&#x7684;&#x662f;&#xff0c;GNU&#x5bf9;UNIX&#x8bf4;&#x4e0d;&#xff0c;YAML&#x8bf4;&#x4e0d;&#x7684;&#x5bf9;&#x8c61;&#x662f;XML&#x3002;YAML&#x4e0d;&#x662f;XML&#x3002;&#x5b83;&#x53ef;&#x4ee5;&#x7528;&#x4f5c;&#x6570;&#x636e;&#x5e8f;&#x5217;&#xff0c;&#x914d;&#x7f6e;&#x6587;&#x4ef6;&#xff0c;log&#x6587;&#x4ef6;&#xff0c;Internat&#x4fe1;&#x606f;&#x548c;&#x8fc7;&#x6ee4;&#x3002;"/>
+</node>
+</node>
+</map>
---
+++ Virtualization/Virtualization.mm
@@ -0,0 +1,44 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1481599618205" ID="ID_457330323" MODIFIED="1481599627474" TEXT="Virtualization">
+<node CREATED="1481599638663" ID="ID_304220420" MODIFIED="1481599645959" POSITION="right" TEXT="&#x5206;&#x7c7b;">
+<node CREATED="1481599706394" ID="ID_1901921501" MODIFIED="1481599717010" TEXT="&#x786c;&#x4ef6;&#x4eff;&#x771f;Emulation">
+<node CREATED="1481599935840" ID="ID_740996722" MODIFIED="1481599942375" TEXT="&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x5f00;&#x53d1;"/>
+<node CREATED="1481599944652" ID="ID_1434800426" MODIFIED="1481599954244" TEXT="&#x56fa;&#x4ef6;&#x53ca;&#x786c;&#x4ef6;&#x7684;&#x5199;&#x4f5c;&#x5f00;&#x53d1;"/>
+</node>
+<node CREATED="1481599719178" ID="ID_1971065728" MODIFIED="1481599731575" TEXT="&#x5168;&#x865a;&#x62df;&#x5316;Full Virtualization">
+<node CREATED="1481599962445" ID="ID_409018707" MODIFIED="1481599975509" TEXT="&#x57fa;&#x672c;&#x539f;&#x7406;">
+<node CREATED="1481599975510" ID="ID_1527821866" MODIFIED="1481600030151" TEXT="&#x5728;&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x548c;&#x786c;&#x4ef6;&#x4e4b;&#x95f4;&#x6355;&#x6349;&#x548c;&#x5904;&#x7406;&#x90a3;&#x4e9b;&#x5bf9;&#x865a;&#x62df;&#x5316;&#x654f;&#x611f;&#x7684;&#x7279;&#x6743;&#x6307;&#x4ee4;&#xff0c;&#x4f7f;&#x5ba2;&#x6237;&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x65e0;&#x9700;&#x4fee;&#x6539;&#x5c31;&#x80fd;&#x8fd0;&#x884c;"/>
+</node>
+<node CREATED="1481600033160" ID="ID_1406421017" MODIFIED="1481600036327" TEXT="&#x4ea7;&#x54c1;">
+<node CREATED="1481600036328" ID="ID_1093463933" MODIFIED="1481600051668" TEXT="VirtualBox"/>
+<node CREATED="1481600053079" ID="ID_1930308482" MODIFIED="1481600055899" TEXT="KVM"/>
+<node CREATED="1481600056971" ID="ID_1213933610" MODIFIED="1481600066246" TEXT="VMware Workstation"/>
+<node CREATED="1481600067643" ID="ID_12221624" MODIFIED="1481600073599" TEXT="VMware ESX"/>
+</node>
+</node>
+<node CREATED="1481599737746" ID="ID_103871416" MODIFIED="1481599755577" TEXT="&#x534a;&#x865a;&#x62df;&#x5316;Paravitualization">
+<node CREATED="1481611657124" ID="ID_1012061343" MODIFIED="1481611660279" TEXT="&#x57fa;&#x672c;&#x539f;&#x7406;">
+<node CREATED="1481611660280" ID="ID_1140279908" MODIFIED="1481611733223" TEXT="&#x5ba2;&#x6237;&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x96c6;&#x6210;&#x7248;&#x865a;&#x62df;&#x5316;&#x7684;&#x6709;&#x5173;&#x7684;&#x4ee3;&#x7801;&#xff0c;&#x975e;&#x5e38;&#x597d;&#x7684;&#x914d;&#x5408;&#x865a;&#x62df;&#x673a;&#x7ba1;&#x7406;&#x7a0b;&#x5e8f;&#x5b8c;&#x6210;&#x865a;&#x62df;&#x5316;"/>
+</node>
+<node CREATED="1481611737278" ID="ID_818632525" MODIFIED="1481611738742" TEXT="&#x7279;&#x70b9;">
+<node CREATED="1481611738743" ID="ID_1113365061" MODIFIED="1481611760746" TEXT="&#x65e0;&#x9700;&#x91cd;&#x65b0;&#x7f16;&#x8bd1;&#x6216;&#x6355;&#x83b7;&#x7279;&#x6743;&#x6307;&#x4ee4;&#xff0c;&#x6027;&#x80fd;&#x975e;&#x5e38;&#x63a5;&#x8fd1;&#x7269;&#x7406;&#x673a;"/>
+<node CREATED="1481611773602" ID="ID_1815386135" MODIFIED="1481611795784" TEXT="&#x9700;&#x8981;&#x5bf9;&#x5ba2;&#x6237;&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x8fdb;&#x884c;&#x4fee;&#x6539;&#xff0c;&#x5728;&#x7528;&#x6237;&#x4f53;&#x9a8c;&#x65b9;&#x9762;&#x6bd4;&#x8f83;&#x9ebb;&#x70e6;"/>
+</node>
+<node CREATED="1481611803330" ID="ID_1677231474" MODIFIED="1481611805729" TEXT="&#x4ea7;&#x54c1;">
+<node CREATED="1481611805730" ID="ID_266169623" MODIFIED="1481611816818" TEXT="Xen"/>
+<node CREATED="1481611819948" ID="ID_1742679401" MODIFIED="1481611825200" TEXT="Hyper-V"/>
+</node>
+</node>
+<node CREATED="1481599756749" ID="ID_810671021" MODIFIED="1481599784257" TEXT="&#x786c;&#x4ef6;&#x8f85;&#x52a9;&#x865a;&#x62df;&#x5316;HardwareAssisted Virtualization">
+<node CREATED="1481611863571" ID="ID_247611612" MODIFIED="1481611867195" TEXT="&#x57fa;&#x672c;&#x539f;&#x7406;">
+<node CREATED="1481611867195" ID="ID_1703341383" MODIFIED="1481611917497" TEXT="Intel&#x548c;AMD&#x7b49;&#x786c;&#x4ef6;&#x5382;&#x5546;&#x901a;&#x8fc7;&#x5bf9;&#x90e8;&#x5206;&#x5168;&#x865a;&#x62df;&#x5316;&#x548c;&#x534a;&#x865a;&#x62df;&#x5316;&#x4f7f;&#x7528;&#x5230;&#x7684;&#x8f6f;&#x4ef6;&#x6280;&#x672f;&#x8fdb;&#x884c;&#x786c;&#x4ef6;&#x8bdd;&#x6765;&#x63d0;&#x9ad8;&#x6027;&#x80fd;"/>
+</node>
+<node CREATED="1481611921162" ID="ID_699644138" MODIFIED="1481611922853" TEXT="&#x7279;&#x70b9;">
+<node CREATED="1481611922854" ID="ID_292736163" MODIFIED="1481611949395" TEXT="&#x5927;&#x91cf;&#x865a;&#x62df;&#x5316;&#x903b;&#x8f91;&#x4ece;&#x8f6f;&#x4ef6;&#x4e2d;&#x5265;&#x79bb;&#xff0c;&#x7b80;&#x5316;&#x4e86;&#x865a;&#x62df;&#x5316;&#x4ea7;&#x54c1;&#x7684;&#x67b6;&#x6784;"/>
+</node>
+</node>
+<node CREATED="1481599797746" ID="ID_1712945973" MODIFIED="1481599817660" TEXT="&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x7ea7;&#x865a;&#x62df;&#x5316;Operating System Level Virtualization"/>
+</node>
+</node>
+</map>
